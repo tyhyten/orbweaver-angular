a13a3fd1b2bd68e2ec1982d424ff2b3efe6992b0
Ty Hyten <ty.hyten@acumendigital.com>
2016-03-17 00:20:31 -0600

    added notes

--- app.js
+++ app.js
@@ -99,17 +99,30 @@
 //Perhaps we change it to be shows happening now up to two weeks from now v
 //Update search by address v
 // Filter by venue?
-// Make loging in and staying logged in work
+// Make loging in and staying logged in work v
 //display success message for playlist creation
-//Pass user location into bandsintown call
+//Pass user location into bandsintown call v
 //Plug user data and city into a backend via API call
 //Disable submit button while playlist is being created to prevent restarting the process
 //Chunk top tracks call into groups of 15 with a timeout between to avoid rate limit. Try doing in app.js rather than method itself because of complexity of using two IIFE's inside of two forEach's
 //Add SASS
-//Add background image
+//Add background image v
 //Shovel failed 429 calls into their own collection and re-run function after timeout
 
 //two options or two apps: one that works for any city and uses random selection of artists, or one for denver for specific venues with all artists
+// - option could be to select venues, which have populated dynamically, once city is entered
+//- or separate playlists for each venue
 
 //deal with page numbers in bandsintown call - limit to venues, filter results by venue
 //add current location option that uses bandsintown location
+
+//NEW PLAN
+
+//OrbWeaver will initially only be for Denver in order to avoid limiting users to a random selection of 50 of 400 artists, by allowing users to select venues.
+//
+//- Grab all events for city from bandsintown, then generate top tracks (and associate with venue name), cache this daily at 12am
+//
+//
+//1. Login
+//2. Select Venues
+//3. Filter cached tracks by venue and create playlist in user's account
