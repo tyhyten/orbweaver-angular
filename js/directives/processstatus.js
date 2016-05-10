562eeb2a64a9da467fc8ddb4ec208f84bd6417e4
Ty Hyten <ty.hyten@acumendigital.com>
2016-05-10 15:09:39 -0600

    Add processStatus directive

---
+++ js/directives/processstatus.js
@@ -0,0 +1,13 @@
+var module = angular.module('OrbWeaver');
+
+module.directive('processStatus', function(){
+  return {
+    restrict: 'E',
+    scope: {},
+    templateUrl: '',
+    link: function (scope) {
+      
+    }
+  }
+
+});
